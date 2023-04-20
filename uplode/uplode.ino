374d80a44da746ccda465f3c42364d7eddf6cf55
waelhamdi12345 <waelhamdi200@gmail.com>
2023-04-20 15:21:02 +0100

    secend comit

--- uplode/uplode.ino
+++ uplode/uplode.ino
@@ -1,63 +1,52 @@
-#include <Adafruit_NeoPixel.h>
-#include <FirebaseESP8266.h>
-#if defined(ESP32)
+
 #include <WiFi.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#endif
+#include <FirebaseESP32.h>
 
-#define WIFI_SSID "phi tien mang" // your wifi SSID
-#define WIFI_PASSWORD "resurrection" //your wifi PASSWORD
-
-#define LedPin 16         // pin d0 as toggle pin
-#define NUM_PIXELS 8      //number of rgb led
-#define PIN_PIXELS 14     //pin d5 as neopixel input
-#define FIREBASE_HOST "toggle-button-3d8-rtdb.firebaseio.com" // change here
-#define FIREBASE_AUTH "icOuLI7Mpe04Nv1ABHfAqyvlQn7HI0aYBwlp"  // your private key
+#define WIFI_SSID "TOPNET_24B8" // your wifi SSID
+#define WIFI_PASSWORD "jxffa6q1zh" //your wifi PASSWORD
+#define FIREBASE_HOST "ijdid-ya-rabe7-default-rtdb.firebaseio.com/" // change here
+#define FIREBASE_AUTH "v9H3OCthc3y6uHUZbI1lpXIlVTcDXkuWvsa3El2A"  // your private key
 FirebaseData firebaseData;
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, PIN_PIXELS, NEO_GRB + NEO_KHZ800);
-void setup ()
-{
-  pinMode(LedPin, OUTPUT);
-  pixels.begin();
+FirebaseJson json;
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  // connect to wifi.
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting");
-  while (WiFi.status() != WL_CONNECTED) {
+
+   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
-    delay(500);
+    delay(300);
   }
   Serial.println();
-  Serial.print("connected: ") ;
+  Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
+  Serial.println();
+
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.reconnectWiFi(true);     
+  Firebase.reconnectWiFi(true);
+  Serial.println("------------------------------------");
+  Serial.println("Connected...");
+
+  // Init SPI bus
+
 }
-void loop ()
-{
-  if(Firebase.getString(firebaseData, "/Led1Status"))
+
+void loop() {
+
+  // put your main code here, to run repeatedly:
+
+ 
+  if (Firebase.getString(firebaseData, "/Led1Status")) // read the value of the actuator 
   {
-    String ledstatus = firebaseData.stringData();
-    if(ledstatus.toInt() == 1){
-      digitalWrite(LedPin, LOW);
-      Serial.println("on");
-      for(int i =0; i<8; i++){
-        pixels.setPixelColor(i, pixels.Color(13, 93, 95));  // #21ecf3 color
-      }
-        pixels.show();
+    String daata = firebaseData.stringData(); 
+    Serial.println(daata);
+
     }
-    else {
-      digitalWrite(LedPin, HIGH);
-      Serial.println("off");
-      for(int i =0; i<8; i++){
-        pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // turn off neopixel
-      }
-        pixels.show();
-    }
-  }else{
-    Serial.print("Error in getInt, ");
-    Serial.println(firebaseData.errorReason());
-  } 
 }
